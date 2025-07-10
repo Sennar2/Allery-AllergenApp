97c3ddb7e7282c67113f448f9ec91c0c600c94f6
Sennar2 <danieleraicaldo@gmail.com>
2025-07-10 22:58:02 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="sticky bottom-0 w-full bg-white border-t border-gray-200 py-3 text-center text-sm text-gray-600 shadow-sm z-10">
-      <p>SafeBite App 2025. Issue with the data displayed? Get in touch.</p>
+      <p>Allerly App 2025. Issue with the data displayed? Get in touch.</p>
     </footer>
   );
 };
