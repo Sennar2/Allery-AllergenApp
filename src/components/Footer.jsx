13f9c3bfc2300099daf558e4f3f1d12a90b3a087
Sennar2 <danieleraicaldo@gmail.com>
2025-07-11 00:05:24 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="sticky bottom-0 w-full bg-white border-t border-gray-200 py-3 text-center text-sm text-gray-600 shadow-sm z-10">
-      <p>Allerly App 2025. Issue with the data displayed? Get in touch.</p>
+      <p>© 2025 Allerly UK. | #EatSafely @getallerly. 
+      Issue with the data displayed? Get in touch.</p>
     </footer>
   );
 };
