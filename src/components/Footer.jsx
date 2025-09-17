4c572529e0df564879673756c5554628df416327
Sennar2 <danieleraicaldo@gmail.com>
2025-09-17 01:42:09 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="sticky bottom-0 w-full bg-white border-t border-gray-200 py-3 text-center text-sm text-gray-600 shadow-sm z-10">
-      <p>© 2025 Allerly UK. | #EatSafely @getallerly. 
-      Data is provided by the venue which is the sole responsible for the allergen logs. Always ask your server.</p>
+      <p>
+      Data is provided by the venue which is the sole responsible for the allergen logs. Always ask your server.
+      © 2025 Allerly UK. | #EatSafely @getallerly. </p>
     </footer>
   );
 };
