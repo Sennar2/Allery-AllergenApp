7df31941cc6223d5ac7a6a1defd76a06a27a58ea
Sennar2 <danieleraicaldo@gmail.com>
2025-09-17 01:41:39 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="sticky bottom-0 w-full bg-white border-t border-gray-200 py-3 text-center text-sm text-gray-600 shadow-sm z-10">
       <p>© 2025 Allerly UK. | #EatSafely @getallerly. 
-      Data are provided by the venue. Always ask your server.</p>
+      Data is provided by the venue which is the sole responsible for the allergen logs. Always ask your server.</p>
     </footer>
   );
 };
